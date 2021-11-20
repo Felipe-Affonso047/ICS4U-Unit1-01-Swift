3abe6fc280cb5c47b18fdf13215bd336c806a849
Ubuntu <ubuntu@ip-172-31-23-127.ec2.internal>
2021-11-20 00:56:55 +0000

    linting done

--- Log.swift
+++ Log.swift
@@ -5,10 +5,11 @@
 * @version 1.0
 * @since   2021-11-18
 */
+
 // Swift "Hello, World!" Program
 
-let LOG_DENSITY: Float = 20
-let TRUCK_CAPACITY: Float = 1100
+let LOGDENSITY: Float = 20
+let TRUCKCAPACITY: Float = 1100
 
 print("What is the length of the logs? (0.25, 0.5, or 1 meter)")
 
@@ -18,7 +19,8 @@ let logLength = readLine()
 // https://stackoverflow.com/questions/24085665/convert-string-to-float-in-swift
 let logLengthFloat = Float(logLength!)
 
-let numberOfLogs = (TRUCK_CAPACITY / logLengthFloat!) / LOG_DENSITY
+let numberOfLogs = (TRUCKCAPACITY / logLengthFloat!) / LOGDENSITY
 
-print("\(numberOfLogs)")
+print("\nThe truck can carry \(numberOfLogs) logs.")
+print("\nDone.\n")
 
